8c92c2db68bcbabfee049870977fa76068df4961
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-08 16:10:42 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -3,4 +3,4 @@ BOT_NAME = "COURSE UPLOADER"
 BOT_TOKEN = ""
 API_ID = 27679306
 API_HASH = "d0f390f798eec4ddb9246a00a44b7a3f"
-MONGO_URI = "mongodb+srv://rowdybro013:DmnZ3s14aRbfA8tb@cluster01.ramjm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster01"
+MONGO_URI = ""
