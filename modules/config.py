2541f79ed752552d3a844a3b3a38d2ab50173f8b
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-05-09 15:14:05 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,10 @@
-# config.py
+import os
+
 BOT_NAME = "COURSE UPLOADER"
-BOT_TOKEN = ""
+
+BOT_TOKEN = os.environ.get("")
+
 API_ID = 27679306
 API_HASH = "d0f390f798eec4ddb9246a00a44b7a3f"
-MONGO_URI = ""
+
+MONGO_URI = os.environ.get("")
